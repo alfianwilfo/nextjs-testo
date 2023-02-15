188bc5cc3dfc3e353dc4f9f0bf4fc9216bf18e93
alfian <alfianwilfredohoris@gmail.com>
2023-02-15 10:19:09 +0700

    navbar

--- gorest/src/components/Navbar.jsx
+++ gorest/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 export default function Navbar() {
   return (
     <>
-      <div className="h-[70px] bg-[#71C9CE]">
+      <div className="h-[70px] bg-[#E3FDFD]">
         <div>
-          <div className="font-bold text-[20px]">Gorest</div>
+          <div className="font-bold text-[20px] text-[#71C9CE]">Gorest</div>
         </div>
       </div>
     </>
