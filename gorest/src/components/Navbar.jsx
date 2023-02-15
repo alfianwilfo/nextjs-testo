1483a11ca082ddc8de9524530d7474754603f5b8
alfian <alfianwilfredohoris@gmail.com>
2023-02-15 16:00:10 +0700

    add router to navbar

--- gorest/src/components/Navbar.jsx
+++ gorest/src/components/Navbar.jsx
@@ -1,9 +1,19 @@
+import { useRouter } from "next/router";
 export default function Navbar() {
+  let router = useRouter();
+  let toHome = () => {
+    router.push("/");
+  };
   return (
     <>
       <div className="h-[70px] bg-[#E3FDFD]">
         <div>
-          <div className="font-bold text-[20px] text-[#71C9CE]">Gorest</div>
+          <button
+            onClick={toHome}
+            className="font-bold text-[20px] text-[#71C9CE]"
+          >
+            Gorest
+          </button>
         </div>
       </div>
     </>
