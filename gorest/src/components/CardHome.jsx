c1c48e24fbb22c5eda71fc3c74a41f6696612b05
alfian <alfianwilfredohoris@gmail.com>
2023-02-15 15:11:12 +0700

    add styling

--- gorest/src/components/CardHome.jsx
+++ gorest/src/components/CardHome.jsx
@@ -1,12 +1,12 @@
 export default function Card({ post, i }) {
   return (
     <>
-      <div className="outline outline-1 outline-[#A6E3E9] px-[10px] hover:shadow-lg hover:shadow-[#E3FDFD]">
+      <div className="transition ease-in-out transition-all duration-700  outline outline-1 outline-[#A6E3E9] px-[10px] hover:shadow-lg hover:shadow-[#E3FDFD]">
         <div>
           <div className="truncate font-bold text-center">{post.title}</div>
         </div>
         <div className="flex justify-center">
-          <button className="outline outline-1 my-[10px] p-[10px]">
+          <button className="transition ease-in-out transition-all duration-700 outline outline-1 my-[10px] p-[5px] hover:rounded ">
             Details
           </button>
         </div>
